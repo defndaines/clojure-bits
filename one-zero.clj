6220c48ddb4b93edaab59113d27b755cca25d505
defndaines <defndaines@gmail.com>
2015-10-25 22:20:27 -0700

    Cleans up one-zero algorithm.

--- one-zero.clj
+++ one-zero.clj
@@ -2,9 +2,9 @@
 
 (defn combine [acc e]
   (if (= e \?)
-    (for [l acc n [\0 \1]] (conj l n))
+    (for [l acc n "01"] (conj l n))
     (for [l acc] (conj l e))))
 
 (defn translate [phrase]
   (map #(apply str %)
-       (reduce combine [(vec "")] (vec phrase))))
+       (reduce combine [[]] phrase)))
