c8df18d9bf90e2e59f50528a00658cc1959a95cc
defndaines <defndaines@gmail.com>
2016-07-14 04:50:03 -0700

    Adds example usage of intern

---
+++ space_age.clj
@@ -0,0 +1,21 @@
+(ns space-age)
+;; Spoiler Alert: This was for the exercism.io "space-age" problem.
+;; Archiving here as a personal example of using intern to create namespaced funtions.
+
+(def periods {"on-mercury" 0.2408467
+              "on-venus" 0.61519726
+              "on-earth" 1.0
+              "on-mars" 1.8808158
+              "on-jupiter" 11.862615
+              "on-saturn" 29.447498
+              "on-uranus" 84.016846
+              "on-neptune" 164.79132})
+
+(defn seconds->years [sec]
+  (/ sec 31557600))
+
+(doseq [[planet period] periods]
+  (intern *ns*
+          (symbol planet)
+          (fn [seconds]
+            (seconds->years (/ seconds period)))))
