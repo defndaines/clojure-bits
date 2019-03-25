2f1f431dd6065c220c6a480b00beaccc535e3543
Michael Daines <953037+defndaines@users.noreply.github.com>
2019-03-25 14:17:09 -0700

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "https://github.com/defndaines/clojure-bits"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.generators "0.1.2"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]]}})
