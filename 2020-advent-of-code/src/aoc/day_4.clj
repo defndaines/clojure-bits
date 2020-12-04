c3a4078bbbeaf962c3683b9cc688ef65a8b939a6
Michael Daines <michaeld@healthfinch.com>
2020-12-04 08:55:33 -0800

    Day 4, Part Two

--- 2020-advent-of-code/src/aoc/day_4.clj
+++ 2020-advent-of-code/src/aoc/day_4.clj
@@ -2,14 +2,80 @@
   "--- Day 4: Passport Processing ---
   ...
   Count the number of valid passports - those that have all required fields.
-  Treat cid as optional. In your batch file, how many passports are valid?"
+  Treat cid as optional. In your batch file, how many passports are valid?
+  --- Part Two ---
+  ...
+  Count the number of valid passports - those that have all required fields
+  and valid values. Continue to treat cid as optional. In your batch file, how
+  many passports are valid?"
   (:require [clojure.string :as string]
             [clojure.set :as set]))
 
 (def required-fields #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"})
 
-(defn passport-valid?
+(defn fields-present?
   [entry]
   (let [kvs (string/split entry #"\s+")
         ks (into #{} (map #(-> % (string/split #":") first)) kvs)]
     (set/subset? required-fields ks)))
+
+(defn- valid-date?
+  [year from to]
+  (when-let [digits (re-find #"^[12][09]\d\d$" year)]
+    (<= from (Integer/parseInt digits) to)))
+
+(defn valid-byr?
+  "A valid birth year has four digits; at least 1920 and at most 2002."
+  [byr]
+  (valid-date? byr 1920 2002))
+
+(defn valid-iyr?
+  "A valid issue year has four digits; at least 2010 and at most 2020."
+  [iyr]
+  (valid-date? iyr 2010 2020))
+
+(defn valid-eyr?
+  "A valid expiration year has four digits; at least 2020 and at most 2030."
+  [eyr]
+  (valid-date? eyr 2020 2030))
+
+(defn valid-hgt?
+  "A valid height is a number followed by either cm or in:
+  - If cm, the number must be at least 150 and at most 193.
+  - If in, the number must be at least 59 and at most 76."
+  [hgt]
+  (when-let [match (re-find #"^(\d{2,3})(cm|in)$" hgt)]
+    (let [v (Integer/parseInt (second match))
+          units (last match)]
+      (case units
+        "cm" (<= 150 v 193)
+        "in" (<= 59 v 76)))))
+
+(defn valid-hcl?
+  "A valid hair color is a # followed by exactly six characters 0-9 or a-f."
+  [hcl]
+  (re-find #"^#\p{XDigit}{6}$" hcl))
+
+(defn valid-ecl?
+  "A valid eye color is exactly one of: amb blu brn gry grn hzl oth."
+  [ecl]
+  (contains? #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} ecl))
+
+(defn valid-pid?
+  "A valid passport ID is a nine-digit number, including leading zeroes."
+  [pid]
+  (re-find #"^\d{9}$" pid))
+
+(defn valid-passport?
+  [entry]
+  (let [kvs (string/split entry #"\s+")
+        passport (into {} (map #(string/split % #":")) kvs)]
+    (and
+     (set/subset? required-fields (set (keys passport)))
+     (valid-byr? (get passport "byr"))
+     (valid-iyr? (get passport "iyr"))
+     (valid-eyr? (get passport "eyr"))
+     (valid-hgt? (get passport "hgt"))
+     (valid-hcl? (get passport "hcl"))
+     (valid-ecl? (get passport "ecl"))
+     (valid-pid? (get passport "pid")))))
--- 2020-advent-of-code/test/aoc/day_4_test.clj
+++ 2020-advent-of-code/test/aoc/day_4_test.clj
@@ -21,26 +21,97 @@ iyr:2011 ecl:brn hgt:59in")
 
 (def input (slurp "resources/day-4-input.txt"))
 
-(deftest passport-valid?-test
+(deftest fields-present?-test
   (testing "valid passport entry, with all entries"
-    (is (day-4/passport-valid?
+    (is (day-4/fields-present?
           "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
           byr:1937 iyr:2017 cid:147 hgt:183cm"))
-    (is (day-4/passport-valid?
+    (is (day-4/fields-present?
           "hcl:#ae17e1 iyr:2013
           eyr:2024
           ecl:brn pid:760753108 byr:1931
           hgt:179cm")))
 
   (testing "invalid passort entires, with missing entries"
-    (is (not (day-4/passport-valid?
+    (is (not (day-4/fields-present?
                "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
                hcl:#cfa07d byr:1929")))
-    (is (not (day-4/passport-valid?
+    (is (not (day-4/fields-present?
                "hcl:#cfa07d eyr:2025 pid:166559648
                iyr:2011 ecl:brn hgt:59in"))))
 
   (testing "valid entries in puzzle input"
     (let [entries (string/split input #"\n\n")]
       (is (= 216
-           (count (filter day-4/passport-valid? entries)))))))
+           (count (filter day-4/fields-present? entries)))))))
+
+(deftest valid-byr?-test
+  (testing "valid birth year"
+    (is (day-4/valid-byr? "2002")))
+  (testing "invalid birth year"
+    (is (not (day-4/valid-byr? "2003")))))
+
+(deftest valid-hgt?-test
+  (testing "valid heights"
+    (is (day-4/valid-hgt? "60in"))
+    (is (day-4/valid-hgt? "190cm")))
+  (testing "invalid heights"
+    (is (not (day-4/valid-hgt? "190in")))
+    (is (not (day-4/valid-hgt? "190")))))
+
+(deftest valid-hcl?-test
+  (testing "valid hair color"
+    (is (day-4/valid-hcl? "#123abc")))
+  (testing "invalid hair colors"
+    (is (not (day-4/valid-hcl? "#123abz")))
+    (is (not (day-4/valid-hcl? "123abc")))))
+
+(deftest valid-ecl?-test
+  (testing "valid eye color"
+    (is (day-4/valid-ecl? "brn")))
+  (testing "invalid eye color"
+    (is (not (day-4/valid-ecl? "wat")))))
+
+(deftest valid-pid?-test
+  (testing "valid passport ID"
+    (is (day-4/valid-pid? "000000001")))
+  (testing "invalid passport ID"
+    (is (not (day-4/valid-pid? "0123456789")))))
+
+(deftest valid-passport?-test
+  (testing "invalid passports"
+    (is (not (day-4/valid-passport?
+               "eyr:1972 cid:100
+               hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926")))
+    (is (not (day-4/valid-passport?
+               "iyr:2019
+               hcl:#602927 eyr:1967 hgt:170cm
+               ecl:grn pid:012533040 byr:1946")))
+    (is (not (day-4/valid-passport?
+               "hcl:dab227 iyr:2012
+               ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277")))
+    (is (not (day-4/valid-passport?
+               "hgt:59cm ecl:zzz
+               eyr:2038 hcl:74454a iyr:2023
+               pid:3556412378 byr:2007"))))
+
+  (testing "valid passports"
+    (is (day-4/valid-passport?
+          "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+          hcl:#623a2f"))
+    (is (day-4/valid-passport?
+          "eyr:2029 ecl:blu cid:129 byr:1989
+          iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm"))
+    (is (day-4/valid-passport?
+          "hcl:#888785
+          hgt:164cm byr:2001 iyr:2015 cid:88
+          pid:545766238 ecl:hzl
+          eyr:2022"))
+    (is (day-4/valid-passport?
+          "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719")))
+
+  (testing "count of valid passports in input"
+    (is (= 150
+           (->> (string/split input #"\n\n")
+                (filter day-4/valid-passport?)
+                count)))))
