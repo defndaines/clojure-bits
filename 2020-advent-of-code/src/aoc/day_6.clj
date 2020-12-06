cccc6a8d9c2f73c2473cdd437fa3c71ca250a33d
Michael Daines <953037+defndaines@users.noreply.github.com>
2020-12-06 10:26:36 -0800

    Day 6, Part Two

--- 2020-advent-of-code/src/aoc/day_6.clj
+++ 2020-advent-of-code/src/aoc/day_6.clj
@@ -2,10 +2,21 @@
   "--- Day 6: Custom Customs ---
   ...
   For each group, count the number of questions to which anyone answered
+  'yes'. What is the sum of those counts?
+  --- Part Two ---
+  ...
+  For each group, count the number of questions to which everyone answered
   'yes'. What is the sum of those counts?"
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.string :as string]
+            [clojure.set :as set]))
 
 (defn group-yes
   "Identify how many unique questions were answered 'yes' in a given group."
   [group]
   (count (remove #{\newline \space} (set group))))
+
+(defn group-all-yes
+  "Identify the questions where everyone in the group answered 'yes'"
+  [group]
+  (let [members (map #(set %) (string/split group #"\n\s*"))]
+    (count (apply set/intersection members))))
--- 2020-advent-of-code/test/aoc/day_6_test.clj
+++ 2020-advent-of-code/test/aoc/day_6_test.clj
@@ -6,6 +6,8 @@
 
 (def input (slurp "resources/day-6-input.txt"))
 
+(def groups (string/split input #"\n\n"))
+
 (deftest group-yes-test
   (testing "yes counts for given groups."
     (is (= 3 (day-6/group-yes "abc")))
@@ -24,4 +26,24 @@
     (is (= 6542
            (reduce
              +
-             (map day-6/group-yes (string/split input #"\n\n")))))))
+             (map day-6/group-yes groups))))))
+
+(deftest group-all-yes-test
+  (testing "counts where all members of group answered yes."
+    (is (= 3 (day-6/group-all-yes "abc")))
+    (is (= 0 (day-6/group-all-yes "a
+                                  b
+                                  c")))
+    (is (= 1 (day-6/group-all-yes "ab
+                                  ac")))
+    (is (= 1 (day-6/group-all-yes "a
+                                  a
+                                  a
+                                  a")))
+    (is (= 1 (day-6/group-all-yes "b"))))
+
+  (testing "sum of all-yes counts across groups."
+    (is (= 3299
+           (reduce
+             +
+             (map day-6/group-all-yes groups))))))
