61a23eca9c2ed2d608bd8e4668bd74952759374f
Michael Daines <michaeld@healthfinch.com>
2020-12-08 07:50:42 -0800

    Day 7, Part Two

--- 2020-advent-of-code/src/aoc/day_7.clj
+++ 2020-advent-of-code/src/aoc/day_7.clj
@@ -1,7 +1,10 @@
 (ns aoc.day-7
   "--- Day 7: Handy Haversacks ---
   ...
-  How many bag colors can eventually contain at least one shiny gold bag?"
+  How many bag colors can eventually contain at least one shiny gold bag?
+  --- Part Two ---
+  ...
+  How many individual bags are required inside your single shiny gold bag?"
   (:require [clojure.string :as string]
             [clojure.set :as set]))
 
@@ -32,3 +35,34 @@
         (if (seq within)
           (recur (set/union acc within) within)
           acc)))))
+
+(defn- parse-rule-with-count
+  [rule]
+  (let [[_ outer inner] (re-find #"^(.*) bags contain (.*)\.$" rule)
+        bags (map
+               #(rest (re-find #"(\d+) (.*) bags?" %))
+               (string/split inner #","))]
+    {outer bags}))
+
+(defn- parse-rules-with-count
+  [rules]
+  (reduce
+    (fn [acc e] (merge acc (parse-rule-with-count e)))
+    {}
+    rules))
+
+(defn bag-count
+  [rules bag cnt]
+  (let [inside (get rules bag)]
+    (reduce
+      (fn [acc [n color]]
+        (if (seq n)
+          (let [amt (* cnt (Integer/parseInt n))]
+            (+ acc amt (bag-count rules color amt)))
+          0))
+      0 inside)))
+
+(defn bag-contains
+  [rules bag]
+  (let [bag-rules (parse-rules-with-count rules)]
+    (bag-count bag-rules bag 1)))
--- 2020-advent-of-code/test/aoc/day_7_test.clj
+++ 2020-advent-of-code/test/aoc/day_7_test.clj
@@ -22,3 +22,18 @@
     (is (= 4 (count (day-7/bag-options sample-rules "shiny gold")))))
   (testing "number of bags based upon input rules"
     (is (= 261 (count (day-7/bag-options input "shiny gold"))))))
+
+(deftest bag-contains-test
+  (testing "number of bags which a given color must hold."
+    (is (= 32 (day-7/bag-contains sample-rules "shiny gold")))
+    (is (= 126
+           (day-7/bag-contains ["shiny gold bags contain 2 dark red bags."
+                                "dark red bags contain 2 dark orange bags."
+                                "dark orange bags contain 2 dark yellow bags."
+                                "dark yellow bags contain 2 dark green bags."
+                                "dark green bags contain 2 dark blue bags."
+                                "dark blue bags contain 2 dark violet bags."
+                                "dark violet bags contain no other bags."]
+                               "shiny gold")))
+    (is (= 3765
+           (day-7/bag-contains input "shiny gold")))))
