bbe88263ab341b297593afb1375dce86f7b1ddba
Michael Daines <953037+defndaines@users.noreply.github.com>
2020-12-19 10:50:10 -0800

    Day 17, Part Two

--- 2020-advent-of-code/src/aoc/day_17.clj
+++ 2020-advent-of-code/src/aoc/day_17.clj
@@ -12,31 +12,44 @@
               (fn [ix y] (map-indexed (fn [iy ch] {[ix iy 0] ch}) y))
               input))))
 
+(defn ->hypercube
+  [input]
+  (reduce merge
+          (flatten
+            (map-indexed
+              (fn [ix y] (map-indexed (fn [iy ch] {[ix iy 0 0] ch}) y))
+              input))))
+
 (defn active [cube]
   (count (filter #(= \# %) (vals cube))))
 
-(def neighbors
+(def neighbors-in-3d
   (filter #(not= [0 0 0] %)
           (for [x [-1 0 1] y [-1 0 1] z [-1 0 1]]
             [x y z])))
 
+(def neighbors-in-4d
+  (filter #(not= [0 0 0 0] %)
+          (for [x [-1 0 1] y [-1 0 1] z [-1 0 1] w [-1 0 1]]
+            [x y z w])))
+
 (defn- get-neighbors
-  [pos]
+  [neighbors pos]
   (map #(mapv + pos %) neighbors))
 
 (defn- update-state
-  [cube pos]
+  [cube neighbors pos]
   (let [current-state (get cube pos)
         active-neighbors (count
-                           (filter #(= \# (get cube %)) (get-neighbors pos)))]
+                           (filter #(= \# (get cube %)) (get-neighbors neighbors pos)))]
     (cond
       (and (not= \# current-state) (= 3 active-neighbors)) \#
       (and (= \# current-state) (< 1 active-neighbors 4)) \#
       :else \.)))
 
-(defn round [cube]
-  (let [all-neighbors (distinct (mapcat get-neighbors (keys cube)))]
-    (into {} (map (fn [pos] [pos (update-state cube pos)]) all-neighbors))))
+(defn round [cube neighbors]
+  (let [all-neighbors (distinct (mapcat (partial get-neighbors neighbors) (keys cube)))]
+    (into {} (map (fn [pos] [pos (update-state cube neighbors pos)]) all-neighbors))))
 
-(defn game [cube]
-  (lazy-seq (cons cube (game (round cube)))))
+(defn game [cube neighbors]
+  (lazy-seq (cons cube (game (round cube neighbors) neighbors))))
--- 2020-advent-of-code/test/aoc/day_17_test.clj
+++ 2020-advent-of-code/test/aoc/day_17_test.clj
@@ -19,13 +19,44 @@
 
 (deftest active-test
   (testing "getting the active count from a cube."
-    (is (= 5 (day-17/active (day-17/->cube sample-input))))))
+    (is (= 5 (day-17/active (day-17/->cube sample-input))))
+    (is (= 5 (day-17/active (day-17/->hypercube sample-input))))))
 
 (deftest game-test
   (testing "progressing the cube a round."
-    (is (= 11 (day-17/active (day-17/round (day-17/->cube sample-input)))))
-    (is (= 11 (day-17/active (nth (day-17/game (day-17/->cube sample-input)) 1))))
-    (is (= 21 (day-17/active (nth (day-17/game (day-17/->cube sample-input)) 2))))
-    (is (= 38 (day-17/active (nth (day-17/game (day-17/->cube sample-input)) 3))))
-    (is (= 112 (day-17/active (nth (day-17/game (day-17/->cube sample-input)) 6))))
-    (is (= 359 (day-17/active (nth (day-17/game (day-17/->cube input)) 6))))))
+    (is (= 11
+           (day-17/active
+             (day-17/round (day-17/->cube sample-input) day-17/neighbors-in-3d))))
+    (is (= 11
+           (day-17/active
+             (nth (day-17/game (day-17/->cube sample-input) day-17/neighbors-in-3d) 1))))
+    (is (= 21
+           (day-17/active
+             (nth (day-17/game (day-17/->cube sample-input) day-17/neighbors-in-3d) 2))))
+    (is (= 38
+           (day-17/active
+             (nth (day-17/game (day-17/->cube sample-input) day-17/neighbors-in-3d) 3))))
+    (is (= 112
+           (day-17/active
+             (nth (day-17/game (day-17/->cube sample-input) day-17/neighbors-in-3d) 6))))
+    (is (= 359
+           (day-17/active
+             (nth (day-17/game (day-17/->cube input) day-17/neighbors-in-3d) 6))))))
+
+(deftest ->hypercube-test
+  (testing "reading input into a hypercube format."
+    (is (= {[0 0 0 0] \. [0 1 0 0] \# [0 2 0 0] \.
+            [1 0 0 0] \. [1 1 0 0] \. [1 2 0 0] \#
+            [2 0 0 0] \# [2 1 0 0] \# [2 2 0 0] \#}
+           (day-17/->hypercube sample-input)))))
+
+(deftest hypercube-test
+  (testing "running the game against a hypercube."
+    (is (= 29 (day-17/active
+                (day-17/round
+                  (day-17/->hypercube sample-input)
+                  day-17/neighbors-in-4d))))
+    #_(is (= 848 (day-17/active
+                 (nth (day-17/game (day-17/->hypercube sample-input) day-17/neighbors-in-4d) 6))))
+    #_(is (= 2228 (day-17/active
+                  (nth (day-17/game (day-17/->hypercube input) day-17/neighbors-in-4d) 6))))))
